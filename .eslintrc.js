3545d952c746d6189ea53e42709150a94766da21
Markus Holopainen <markusp_holopainen@yahoo.com>
2022-06-28 15:26:10 +0300

    4.3

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,6 +3,7 @@ module.exports = {
     node: true,
     commonjs: true,
     es2021: true,
+    eslint: true,
   },
   extends: 'eslint:recommended',
   parserOptions: {
---
+++ utils/list_helper.js
@@ -0,0 +1,8 @@
+//eslint-disable-next-line
+const dummy = (blogs) => {
+  return 1
+}
+
+module.exports = {
+  dummy,
+}
