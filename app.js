f2cbe2af2a9d9b65885de186aa531f922e57c248
Markus Holopainen <markusp_holopainen@yahoo.com>
2022-10-31 15:01:56 +0200

    5.17 backend

--- app.js
+++ app.js
@@ -31,6 +31,10 @@ app.use(tokenExtractor)
 app.use('/api/blogs', userExtractor, blogsRouter)
 app.use('/api/users', usersRouter)
 app.use('/api/login', loginRouter)
+if (process.env.NODE_ENV === 'test') {
+  const testingRouter = require('./controllers/testing')
+  app.use('/api/testing', testingRouter)
+}
 
 app.get('/hello', (req, res) => {
   info('hello?', req, res)
